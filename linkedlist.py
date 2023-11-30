b8009f641774e374ef411d81334402f612d327b8
edsonibarra <edson.ibarra@globant.com>
2023-11-30 16:04:14 -0600

    node

---
+++ linkedlist.py
@@ -0,0 +1,12 @@
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        pass
+    
+    def append(self, data):
+        """
+        Add a new node at the end of the list.
+        """
+        pass
