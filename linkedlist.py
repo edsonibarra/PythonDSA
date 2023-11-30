4556694a5c80f2c6d4f65f9717e4c11d9ecdd019
edsonibarra <edsonibarra97@gmail.com>
2023-11-30 16:17:40 -0600

    handle case where linked list is not empty in append method

--- linkedlist.py
+++ linkedlist.py
@@ -16,3 +16,7 @@ class LinkedList:
         if self.head is None:
             self.head = new_node
             return
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = current_node
